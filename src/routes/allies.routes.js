18f67193238d635cb62fc588fd5ac785854935f8
felixDubex <1338749@cstj.qc.ca>
2025-11-19 10:54:21 -0500

    changement allies.routes pour fit mockoon

--- src/routes/allies.routes.js
+++ src/routes/allies.routes.js
@@ -9,8 +9,8 @@ import { guardAuthorizationJWT } from '../middlewares/authorization.jwt.js';
 const router = express.Router();
 
 
-router.get('/:uuid', guardAuthorizationJWT, retrieveAlliesByUUID);
-router.get('/:uuidAlly', guardAuthorizationJWT, retrieveOneAlly);
+router.get('/:uuid/allies', guardAuthorizationJWT, retrieveAlliesByUUID);
+router.get('/:uuid/allies/:uuidAlly', guardAuthorizationJWT, retrieveOneAlly);
 router.patch('/:uuid', guardAuthorizationJWT, addAlly);
 
 
