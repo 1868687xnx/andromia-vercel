f4d86bb2396b4794fbc0c2f183ec17ebf1136ea3
1868687xnx <1868687@cstj.qc.ca>
2025-11-26 10:54:19 -0500

    added changes

--- src/repositories/ally.repository.js
+++ src/repositories/ally.repository.js
@@ -1,65 +1,69 @@
-import { Ally } from '../models/ally.model.js';
-
+import { Ally } from "../models/ally.model.js";
 
 class AllyRepository {
-    retrieveByUUID(uuid) {
-        return Ally.find({uuid: uuid});
+  retrieveByUUID(uuid) {
+    return Ally.find({ uuid: uuid });
+  }
+
+  retrieveForOneUser(idExplorateur) {
+    const retrieveQuery = Ally.find(options.filter)
+      .limit(options.limit)
+      .skip(options.skip)
+      .sort({ explorationDate: 1 });
+    return Promise.all([
+      retrieveQuery,
+      Exploration.countDocuments(options.filter),
+    ]);
+  }
+
+  transform(ally) {
+    delete ally.books;
+    delete ally.expireAt;
+    delete ally.crypto;
+    return ally;
+  }
+
+  async createForOneUser(allyUUID, explorateur_id) {
+    console.log("ALLY UUID REPO :", allyUUID);
+    let newally = await Ally.findOne({ uuid: allyUUID });
+    console.log("NEW ALLY :", newally);
+    if (!newally) {
+      throw new Error("Ally not found");
     }
+    const updatedAllyData = this.transform(newally.toObject(), explorateur_id);
+    await Ally.findOneAndUpdate({ uuid: allyUUID }, updatedAllyData, {
+      new: true,
+    });
+    // Fetch the updated Ally
+    const updatedAlly = await Ally.findOne({ uuid: allyUUID });
+    return updatedAlly;
+  }
 
-    retrieveForOneUser(idExplorateur) {
-        const allies = Ally.find({explorateur: idExplorateur});
-
-        console.log(allies);
-        return allies;
-    }
-
-    transform(ally) {
-        delete ally.books;
-        delete ally.expireAt;
-        delete ally.crypto;
-        return ally;
-    }
-
-    async createForOneUser(allyUUID, explorateur_id) {
-        console.log("ALLY UUID REPO :", allyUUID);
-        let newally = await Ally.findOne({uuid: allyUUID});
-        console.log("NEW ALLY :", newally);
-        if (!newally) {
-            throw new Error("Ally not found");
+  // Méthode pour générer un ally aléatoire via l'API externe
+  async generateRandomAlly(explorateur_id) {
+    try {
+      const response = await fetch(
+        "https://api.andromia.science/allies/actions?type=generate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        const updatedAllyData = this.transform(newally.toObject(), explorateur_id);
-        await Ally.findOneAndUpdate(
-            { uuid: allyUUID },
-            updatedAllyData,
-            { new: true }
-        );
-        // Fetch the updated Ally
-        const updatedAlly = await Ally.findOne({uuid: allyUUID});
-        return updatedAlly;
-    }
+      );
 
-    // Méthode pour générer un ally aléatoire via l'API externe
-    async generateRandomAlly(explorateur_id) {
-        try {
-            const response = await fetch('https://api.andromia.science/allies/actions?type=generate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            
-            if (response.ok) {
-                const allyData = await response.json();
-                // Créer l'ally pour cet utilisateur
-                await this.createForOneUser(allyData.uuid, explorateur_id);
-                return true;
-            }
-            return false;
-        } catch (err) {
-            console.error("Erreur lors de la génération de l'ally:", err);
-            return false;
-        }
+      if (response.ok) {
+        const allyData = await response.json();
+        // Créer l'ally pour cet utilisateur
+        await this.createForOneUser(allyData.uuid, explorateur_id);
+        return true;
+      }
+      return false;
+    } catch (err) {
+      console.error("Erreur lors de la génération de l'ally:", err);
+      return false;
     }
+  }
 }
 
 export default new AllyRepository();
--- src/routes/explorateurs.routes.js
+++ src/routes/explorateurs.routes.js
@@ -1,152 +1,194 @@
-import express from 'express';
-import HttpErrors from 'http-errors';
-import validator from './../middlewares/validator.js';
-import explorateurRepository from "../repositories/explorateur.repository.js"
-import allyRepository from '../repositories/ally.repository.js';
-import explorateurValidators from '../validators/explorateur.validator.js';
-import { guardAuthorizationJWT } from '../middlewares/authorization.jwt.js';
-import { TABLE_ELEMENT } from '../core/constants.js';
+import express from "express";
+import HttpErrors from "http-errors";
+import validator from "./../middlewares/validator.js";
+import explorateurRepository from "../repositories/explorateur.repository.js";
+import allyRepository from "../repositories/ally.repository.js";
+import explorateurValidators from "../validators/explorateur.validator.js";
+import { guardAuthorizationJWT } from "../middlewares/authorization.jwt.js";
+import { TABLE_ELEMENT } from "../core/constants.js";
+import paginateMiddleware from "../middlewares/paginate.js";
 
 const router = express.Router();
 
-router.post('/', explorateurValidators.postValidator(), validator, post);
-router.get('/:uuid', guardAuthorizationJWT, retrieveOne);
-router.get('/:uuid/vault', guardAuthorizationJWT, retrieveVault);
-router.get('/:uuid/allies/', guardAuthorizationJWT, retrieveAlliesByUUID);
-router.get('/:uuid/allies/:uuidAlly', guardAuthorizationJWT, retrieveOneAlly);
-router.patch('/allies/:uuid', guardAuthorizationJWT, addAlly);
-router.post('/:uuid/openlootbox', guardAuthorizationJWT, openLootbox);
+router.post("/", explorateurValidators.postValidator(), validator, post);
+router.get("/:uuid", guardAuthorizationJWT, retrieveOne);
+router.get("/:uuid/vault", guardAuthorizationJWT, retrieveVault);
+router.get("/:uuid/allies/", paginateMiddleware({ defaultLimit: 25, defaultMaxLimit: 50 }), guardAuthorizationJWT, retrieveAlliesByUUID);
+router.get("/:uuid/allies/:uuidAlly", guardAuthorizationJWT, retrieveOneAlly);
+router.patch("/allies/:uuid", guardAuthorizationJWT, addAlly);
+router.post("/:uuid/openlootbox", guardAuthorizationJWT, openLootbox);
 
 async function post(req, res, next) {
-    try {
-        let account = await explorateurRepository.create(req.body);
-        const tokens = explorateurRepository.generateJWT(account.uuid);
-        account = account.toObject({ getters: false, virtuals: false });
-        account = explorateurRepository.transform(account);
-        res.status(201).json({ account, tokens });
-    } catch (err) {
-        return next(err);
-    }
+  try {
+    let account = await explorateurRepository.create(req.body);
+    const tokens = explorateurRepository.generateJWT(account.uuid);
+    account = account.toObject({ getters: false, virtuals: false });
+    account = explorateurRepository.transform(account);
+    res.status(201).json({ account, tokens });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 async function retrieveOne(req, res, next) {
-    try {
-        let account = await explorateurRepository.retrieveByUUID(req.params.uuid);
-        if (!account) {
-            return next(HttpErrors.NotFound());
-        } else {
-            account = account.toObject({ getters: false, virtuals: false });
-            account = explorateurRepository.transform(account);
-            res.status(200).json(account);
-        }
-    } catch (err) {
-        return next(err);
+  try {
+    let account = await explorateurRepository.retrieveByUUID(req.params.uuid);
+    if (!account) {
+      return next(HttpErrors.NotFound());
+    } else {
+      account = account.toObject({ getters: false, virtuals: false });
+      account = explorateurRepository.transform(account);
+      res.status(200).json(account);
     }
+  } catch (err) {
+    return next(err);
+  }
 }
 
 // Route pour retrieve le vault d'un explorateur
 async function retrieveVault(req, res, next) {
-    try {
-        let account = await explorateurRepository.retrieveByUUID(req.params.uuid);
-        if (!account) {
-            return next(HttpErrors.NotFound());
-        } else {
-            // on retourne le vault de l'explorateur
-            const vault = account.inventory.vault;
-            res.status(200).json(vault);
-        }
-    } catch (err) {
-        return next(err);
+  try {
+    let account = await explorateurRepository.retrieveByUUID(req.params.uuid);
+    if (!account) {
+      return next(HttpErrors.NotFound());
+    } else {
+      // on retourne le vault de l'explorateur
+      const vault = account.inventory.vault;
+      res.status(200).json(vault);
     }
+  } catch (err) {
+    return next(err);
+  }
 }
 
 async function addAlly(req, res, next) {
-    try {
-        let explorateur = await explorateurRepository.retrieveByUUID(req.auth.uuid);
-        console.log("Explorateur dans allies.routes.js :", req.auth.uuid);
-        let newAlly = await allyRepository.createForOneUser(req.params.uuid, explorateur._id);
+  try {
+    let explorateur = await explorateurRepository.retrieveByUUID(req.auth.uuid);
+    console.log("Explorateur dans allies.routes.js :", req.auth.uuid);
+    let newAlly = await allyRepository.createForOneUser(
+      req.params.uuid,
+      explorateur._id
+    );
 
-        if (req.query._body === 'false') {
-            return res.status(204).end();
-        }
-        newAlly = newAlly.toObject({ getters: false, virtuals: false });
-        res.status(201).json(newAlly);
-    } catch (err) {
-        return next(err);
+    if (req.query._body === "false") {
+      return res.status(204).end();
     }
+    newAlly = newAlly.toObject({ getters: false, virtuals: false });
+    res.status(201).json(newAlly);
+  } catch (err) {
+    return next(err);
+  }
 }
 
 // Route pour récupérer les Allies d'un explorateur spécifique par son UUID
 async function retrieveAlliesByUUID(req, res, next) {
-    try {
-        const explorateur = await explorateurRepository.retrieveByUUID(req.params.uuid);
-        if (!explorateur) {
-            return next(HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID"));
-        }
+  try {
+    const explorateur = await explorateurRepository.retrieveByUUID(
+      req.params.uuid
+    );
+    if (!explorateur) {
+      return next(
+        HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID")
+      );
+    } else {
+      const options = {
+        skip: req.pagination.skip,
+        limit: req.pagination.limit,
+        filter: { explorateur },
+      };
+      let [allies, totalDocuments] = await allyRepository.retrieveForOneUser(
+        explorateur._id,
+        options
+      );
+      allies = allies.map((a) => {
+        a = a.toObject({ getters: false, virtuals: false });
+        a = allyRepository.transform(a, req.options);
+        return a;
+      });
+      const totalPages = Math.ceil(totalDocuments / req.pagination.limit);
 
-        let allies = await allyRepository.retrieveForOneUser(explorateur._id);
+      if (req.pagination.page > totalPages) {
+        throw HttpErrors.BadRequest(
+          `La page demandée doit être inférieure à ${totalPages}`
+        );
+      }
 
-        allies = allies.map(a => {
-            a = a.toObject({ getters: false, virtuals: false });
-            a = allyRepository.transform(a, req.options);
-            return a;
-        });
-
-        res.status(200).json(allies);
-    } catch (err) {
-        return next(err);
+      const responseBody = {
+        _metadata: {
+          page: req.pagination.page,
+          limit: req.pagination.limit,
+          skip: req.pagination.skip,
+          hasNextPage: req.pagination.page < totalPages,
+          totalPages: totalPages,
+          totalDocuments: totalDocuments,
+        },
+        _links: req.pagination.links(totalPages),
+        data: explorations,
+      };
     }
+    res.status(200).json(allies);
+  } catch (err) {
+    return next(err);
+  }
 }
-
 
 // Route pour récupérer un ally spécifique d'un explorateur par son UUID
 async function retrieveOneAlly(req, res, next) {
-    try {
-        const explorateur = await explorateurRepository.retrieveByUUID(req.params.uuid);
-        if (!explorateur) {
-            return next(HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID"));
-        }
-
-        let ally = await allyRepository.retrieveByUUID(req.params.allyUUID);
-        if (!ally) {
-            return next(HttpErrors.NotFound("Aucun ally trouvé avec cet UUID"));
-        }
-
-        res.status(200).json(ally);
-    } catch (err) {
-        return next(err);
+  try {
+    const explorateur = await explorateurRepository.retrieveByUUID(
+      req.params.uuid
+    );
+    if (!explorateur) {
+      return next(
+        HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID")
+      );
     }
+
+    let ally = await allyRepository.retrieveByUUID(req.params.allyUUID);
+    if (!ally) {
+      return next(HttpErrors.NotFound("Aucun ally trouvé avec cet UUID"));
+    }
+
+    res.status(200).json(ally);
+  } catch (err) {
+    return next(err);
+  }
 }
 
-// Route pour ouvrir un lootbox d'un explorateur 
+// Route pour ouvrir un lootbox d'un explorateur
 async function openLootbox(req, res, next) {
-    try {
-        const explorateur = await explorateurRepository.retrieveByUUID(req.params.uuid);
-        if (!explorateur) {
-            return next(HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID"));
-        }
-
-        // Ouvrir la lootbox et récupérer les récompenses
-        const lootboxResult = await explorateurRepository.openLootbox(explorateur, TABLE_ELEMENT);
-
-        // On fait un randomize pour savoir si on ajoute un ally ou pas (50% de chance)
-        const addAllyChance = Math.random();
-        let allyAdded = false;
-        if (addAllyChance > 0.5) {
-            // on ajoute un ally via le repository
-            allyAdded = await allyRepository.generateRandomAlly(explorateur._id);
-        }
-
-        // Retourner le résultat
-        res.status(200).json({
-            ...lootboxResult,
-            allyAdded: allyAdded
-        });
-    } catch (err) {
-        return next(err);
+  try {
+    const explorateur = await explorateurRepository.retrieveByUUID(
+      req.params.uuid
+    );
+    if (!explorateur) {
+      return next(
+        HttpErrors.NotFound("Aucun explorateur trouvé avec cet UUID")
+      );
     }
+
+    // Ouvrir la lootbox et récupérer les récompenses
+    const lootboxResult = await explorateurRepository.openLootbox(
+      explorateur,
+      TABLE_ELEMENT
+    );
+
+    // On fait un randomize pour savoir si on ajoute un ally ou pas (50% de chance)
+    const addAllyChance = Math.random();
+    let allyAdded = false;
+    if (addAllyChance > 0.5) {
+      // on ajoute un ally via le repository
+      allyAdded = await allyRepository.generateRandomAlly(explorateur._id);
+    }
+
+    // Retourner le résultat
+    res.status(200).json({
+      ...lootboxResult,
+      allyAdded: allyAdded,
+    });
+  } catch (err) {
+    return next(err);
+  }
 }
-
-
 
 export default router;
